4597433bad6a30ca39f19944a304f44d52ef6197
Francisco Javier González <franciscojgonzalezfernandez@gmail.com>
2026-02-01 00:49:41 +0100

    First calendar version so Ugly

--- src/calendar/pages/CalendarPage.tsx
+++ src/calendar/pages/CalendarPage.tsx
@@ -1,9 +1,41 @@
+import { Calendar, dateFnsLocalizer } from "react-big-calendar";
+import type { Event } from "react-big-calendar";
+import { format, parse, startOfWeek, getDay, addHours } from "date-fns";
+import { enUS } from "date-fns/locale/en-US";
+
 import { NavBar } from "../components/NavBar";
+
+const locales = {
+  "en-US": enUS,
+};
+
+const localizer = dateFnsLocalizer({
+  format,
+  parse,
+  startOfWeek,
+  getDay,
+  locales,
+});
+
+const myEventsList: Event[] = [
+  {
+    title: "Javi's Birthday",
+    start: new Date(),
+    end: addHours(new Date(), 1),
+  },
+];
 
 export const CalendarPage = () => {
   return (
     <>
       <NavBar />
+      <Calendar
+        localizer={localizer}
+        events={myEventsList}
+        startAccessor="start"
+        endAccessor="end"
+        style={{ height: 500 }}
+      />
     </>
   );
 };
