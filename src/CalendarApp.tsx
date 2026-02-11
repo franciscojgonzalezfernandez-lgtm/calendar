e13a5810cbb280f05a9611a1c6dfc10de6bb2c37
Francisco Javier González <franciscojgonzalezfernandez@gmail.com>
2026-02-11 23:58:29 +0100

    Reducer in.

--- src/CalendarApp.tsx
+++ src/CalendarApp.tsx
@@ -1,6 +1,8 @@
 import { BrowserRouter } from "react-router";
 import { AppRouter } from "./routes";
 
+import { Provider } from "react-redux";
+
 export const CalendarApp = () => {
   return (
     <BrowserRouter>
